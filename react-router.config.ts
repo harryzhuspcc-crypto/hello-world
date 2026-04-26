de7c0765c83c0f7906b7c02b95ed7a1d088a6eb2
harry <harry.zhu.spcc@gmail.com>
2026-04-26 15:44:08 +1000

    feat: React Router v7 + Cloudflare Workers setup

---
+++ react-router.config.ts
@@ -0,0 +1,9 @@
+// react-router.config.ts
+import type { Config } from "@react-router/dev/config";
+
+export default {
+  ssr: true,
+  future: {
+    v8_viteEnvironmentApi: true,
+  },
+} satisfies Config;
